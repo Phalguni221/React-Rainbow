0f454aec1c4326a1c4d08836a5f9b339509ef104
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-28 17:38:13 -0500

    React-Rainbow

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, {useState} from 'react'
 
+import './App.css'
 
 import ColorBlock from './ColorBlock'
 
 import ColorForm from './ColorForm'
 
-// import ColorForm from './ColorForm'
 
-
+<div>
+<h1>Enter a valid color name below and see it appear on the screen! Magic!!</h1>
+</div>
 function App(){
     let [colors, setColors] = useState ([
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
+    //Commented out for Bonus Section (As if starting app with blank array - user can make any type of rainbow using any color!)
+        // 'violet', 'blue',
+        // 'lightblue', 'green',
+        // 'greenyellow', 'yellow',
+        // 'orange', 'red'
     ])
     
     let colorMap = colors.map((color, i) => {
--- src/ColorForm.js
+++ src/ColorForm.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 
+
 function ColorForm(props){
     let [input, setInput] = useState('')
 
     const handleSubmit = (e) => {
         // We still want to eliminate the default behavior of our form submittal
         e.preventDefault()
+        setInput("")
         // addColor, the function we just built, should be available within props.
         props.addColor(input)
     }
 
     return (
         <div>
+            <h1>Enter a valid color name and watch it appear on the screen! Magic!!</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" 
-                onChange={(e) => setInput(e.target.value)} />
+                <input type="text" minLength="3" maxLength="20" 
+                onChange={(e) => setInput(e.target.value)}
+                value={input} />
                 <button type="submit">Submit!</button>
             </form>
         </div>
